6c65ba72d067d5849df0128851c08f3b95de3141
Giovanni Tshiabngu <61130107+Moodkiller20@users.noreply.github.com>
2022-07-06 12:26:28 -0700

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,14 @@
+key={
+'HOST':'192.254.186.163',
+'USER':'giosroom_pyGio',
+'PASSWORD':'myFord#05',
+'DATABASE':"giosroom_outsideTest"
+}
+
+MAIL = {
+    'PORT':587,
+    'SERVER':"smtp.gmail.com",
+    'EMAIL':"giosroom@gmail.com",
+    'EMAIL_TO':"giovannitshibangu30@gmail.com",
+    'PASSWORD':"mehwxtqidrwkdztl"
+}
