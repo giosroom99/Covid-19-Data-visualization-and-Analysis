4dfbd54972362d9bfaa91876c96cd6e144ee25c0
Giovanni Tshiabngu <61130107+Moodkiller20@users.noreply.github.com>
2022-07-20 15:49:08 -0400

    Update covid19_new_data.py

--- covid19_new_data.py
+++ covid19_new_data.py
@@ -7,6 +7,7 @@ import calendar
 from secret import key
 from mail import send_update
 from logFile import Status_Report
+from vaccine import vaccine_data
 
 today = datetime.datetime.today() # Get today time.
 
@@ -16,22 +17,22 @@ def driver():
 
     ###     ----Variables---
 
-    
+
     real_time_url ="https://covid-api.mmediagroup.fr/v1/cases?country="
     countries = get_countries() # Get a list of countries.
-    
+
     mydb =db_connect()  # Connect to DB
     print(mydb) # Print DB object
 
     realtime(real_time_url,mydb,countries,today) # This function collect real time data from API
-    Status_Report("Finish"+today,"ALL Fectched")
-    send_update("Finish"+today,"ALL Fectched")
-    
+    Status_Report("Finish"+str(today),"ALL Fectched")
+    send_update("Finish"+str(today),"ALL Fectched")
+
 
 
 
 def get_countries():
-    
+
     url = "https://countriesnow.space/api/v0.1/countries"
     lit_of_countries = []
     try:
@@ -44,7 +45,8 @@ def get_countries():
             country_data = parse_json['data']
             for c in country_data:
                 lit_of_countries.append(c['country'])
-           
+
+
 
             message="Countries Fetched"
             code ="Success"
@@ -55,9 +57,9 @@ def get_countries():
             code ="Error "+status
             Status_Report(code,message)
             send_update(code,message)
-            
-        
-    except BaseException as error: 
+
+
+    except BaseException as error:
         print("An error has occured while trying to get country list.",error)
         code =error
         message="An error has occured while trying to get country list."
@@ -91,7 +93,7 @@ def db_connect():
 def realtime(real_time_url,mydb,countries,today):
     for country in  countries:
         response = requests.get(real_time_url+country)
-        response = requests.get(url)
+
         status = response.status_code
         reason = response.reason
         data = response.text
@@ -99,6 +101,7 @@ def realtime(real_time_url,mydb,countries,today):
         if(status ==200):
             try:
                 parse_json = json.loads(data)
+                # Get covid data    ########################################################################################################################
                 all = parse_json["All"]
 
                 deaths = all["deaths"]
@@ -106,11 +109,19 @@ def realtime(real_time_url,mydb,countries,today):
                 recovered = all["recovered"]
                 population = all["population"]
                 Continent = all["continent"]
-                print("Date:{6}, Continent: {0}, Contry: {1}, Population: {2}, Confirmed: {3}, Deaths: {4}, Recovered: {5}".format(Continent,country,population,confirmed,recovered,deaths, today))
+
+                # Get Vaccine Data  ########################################################################################################################
+                VC = vaccine_data(country) # imported
+
+                print("Date:{9}, Continent: {0}, Contry: {1}, Population: {2}, Confirmed: {3}, Deaths: {4}, Recovered: {5}, administered: {6}, people_vaccinated: {7}, people_partially_vaccinated: {8}".format(Continent,country,population,confirmed,deaths,recovered,VC[0],VC[1],VC[2],today))
+
+
+
+
 
                 try:
-                    sql = 'INSERT INTO covid_data (Last_updated,Continent, country,population, deaths, confirmed, recorvered) VALUES (%s,%s,%s,%s,%s,%s,%s)'
-                    val=(today,Continent,country,population,deaths,confirmed,recovered)
+                    sql = 'INSERT INTO covid_data (Last_updated,Continent, country,population, deaths, confirmed, recorvered,administered,people_vaccinated,people_partially_vaccinated) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+                    val=(today,Continent,country,population,deaths,confirmed,recovered,VC[0],VC[1],VC[2])
                     mycursor = mydb.cursor()
                     mycursor.execute(sql,val)
                     mydb.commit()
@@ -122,20 +133,19 @@ def realtime(real_time_url,mydb,countries,today):
                     message=errdb
                     Status_Report(code,message)
                     send_update(code,message)
-        
+
             except KeyError:
                 print(KeyError,' for ',country)
                 code="Error in country{0}".format(country)
                 message=KeyError
                 Status_Report(code,message)
                # send_update(code,message)
-               
+
         else:
             message="API request error: "+reason
             code ="Error "+status
             Status_Report(code,message)
             send_update(code,message)
-            
 
 
 driver()
